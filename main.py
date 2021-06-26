8264a13ad9145dd4ff25c0a436f0ca50917533af
DarkArtheme <artyom.bogdanov.2012@yandex.ru>
2021-06-27 02:54:32 +0300

    Теперь у музеев отображается, находятся ли они в избранном у данного пользователя

--- main.py
+++ main.py
@@ -32,19 +32,28 @@ async def start_page():
 
 @app.post("/get_favorites")
 async def get_favorites_by_id(data: UserId):
+    """Возвращает список id избранных музеев у данного пользователя."""
     result = users_coll.find_one({"UserId": data.user_id})
-    favorites = []
     if result is None:
         users_coll.insert_one({"UserId": data.user_id, "Favorites": []})
         return []
     else:
-        for fav in result["Favorites"]:
-            favorites.append(fav)
-        return favorites
+        return list(result["Favorites"])
+
+
+def get_favourites_by_id_sync(user_id):
+    """То же самое что и get_favorites_by_id, только не асинхронная и на вход поступает не схема"""
+    result = users_coll.find_one({"UserId": user_id})
+    if result is None:
+        users_coll.insert_one({"UserId": user_id, "Favorites": []})
+        return []
+    else:
+        return list(result["Favorites"])
 
 
 @app.post("/add_to_favorites")
 async def add_to_favorites(data: FavMuseum):
+    """Добавляет музей в избранное данному пользователю."""
     result = users_coll.find_one({"UserId": data.user_id})
     if result is None:
         users_coll.insert_one({"UserId": data.user_id, "Favorites": []})
@@ -53,6 +62,7 @@ async def add_to_favorites(data: FavMuseum):
 
 @app.post("/delete_from_favorites")
 async def delete_from_favorites(data: FavMuseum):
+    """Удаляет музей из избранного данному пользователю."""
     result = users_coll.find_one({"UserId": data.user_id})
     if result is None:
         return []
@@ -60,33 +70,46 @@ async def delete_from_favorites(data: FavMuseum):
 
 
 # Retrieve all museums present in the database
-@app.get("/museums")
-async def get_all_museums():
+@app.post("/museums")
+async def get_all_museums(data: UserId):
+    """Возвращает список всех музеев в бд. На вход принимает user_id, чтобы определить, какие из музеев в избранном."""
     museums = []
     for museum in museums_collection.find().sort("_id", 1):
-        museums.append(museum_short(museum))
+        museums.append(museum_short(museum, data.user_id))
     return museums
 
 
 @app.post("/museums/by_id")
 async def get_museum(data: MuseumId):
+    """Возвращает информацию о конкретном музее по его id."""
     museum = museums_collection.find_one({"_id": data.museum_id})
     if museum:
-        return museum_helper(museum)
+        return museum_helper(museum, data.user_id)
 
 
 @app.post("/favorites")
 async def get_favorites_list(data: UserId):
+    """Возвращает список избранных музеев у данного пользователя."""
     fav_list = []
     fav = await get_favorites_by_id(data)
     for museum in museums_collection.find().sort("_id", 1):
         for i in fav:
             if museum["_id"] == i:
-                fav_list.append(museum_helper(museum))
+                fav_list.append(museum_helper(museum, data.user_id))
     return fav_list
 
 
-def museum_helper(museum) -> dict:
+def is_in_favourites(user_id, museum_id):
+    """Проверка, есть ли музей с данным id в избранном у пользователя с данным id"""
+    fav = get_favourites_by_id_sync(user_id)
+    for fav_id in fav:
+        if museum_id == fav_id:
+            return True
+    return False
+
+
+def museum_helper(museum, user_id) -> dict:
+    """Полная информация о данном музее"""
     return {
         "id": museum["_id"],
         "name": museum["name"],
@@ -105,10 +128,12 @@ def museum_helper(museum) -> dict:
         "distance": museum["distance"],
         "station": museum["station"],
         "payment": museum["payment"],
+        "in_favourites": is_in_favourites(user_id, museum["_id"])
     }
 
 
-def museum_short(museum) -> dict:
+def museum_short(museum, user_id) -> dict:
+    """Краткая информация о данном музее"""
     return {
         "id": museum["_id"],
         "name": museum["name"],
@@ -119,6 +144,7 @@ def museum_short(museum) -> dict:
         "distance": museum["distance"],
         "station": museum["station"],
         "payment": museum["payment"],
+        "in_favourites": is_in_favourites(user_id, museum["_id"])
     }
 
 
